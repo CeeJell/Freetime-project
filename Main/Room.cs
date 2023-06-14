4fbcad4dc676fe43380e4173147ab0eec80b37a5
Charlie Larsson <charlie.larsson@realgymnasiet.se>
2023-06-14 13:09:46 +0200

    Add project files.

---
+++ Main/Room.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Room
+    {
+
+        private Room? north;
+        private Room? south;
+        private Room? east;
+        private Room? west;
+
+        public Loot loot;
+
+        public string Description { get; set; } = "";
+
+        public void AddNorth(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom.south = this;
+            north = otherRoom;
+        }
+        public void AddSouth(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom.north = this;
+            south = otherRoom;
+        }
+        public void AddEast(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom.west = this;
+            east = otherRoom;
+        }
+        public void AddWest(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom.east = this;
+            west = otherRoom;
+        }
+    }
+
+    public enum Loot
+    {
+
+    }
+
+}
